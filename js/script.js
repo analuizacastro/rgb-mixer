6ec8891fc84c75141d31c872c731918f0ab4ef2a
Ana Luiza Castro <analuizabcastro@gmail.com>
2020-06-19 20:56:57 -0300

    chore: Title correction and files organization

---
+++ js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", render());
+
+function render() {
+  let red = document.getElementById("red").value;
+  let green = document.getElementById("green").value;
+  let blue = document.getElementById("blue").value;
+
+  let outputR = document.querySelector("#outputR");
+  let outputG = document.querySelector("#outputG");
+  let outputB = document.querySelector("#outputB");
+
+  outputR.textContent = red;
+  outputG.textContent = green;
+  outputB.textContent = blue;
+
+  let rgb = `rgb(${red}, ${green}, ${blue})`;
+  document.querySelector(".square-1").style.background = rgb;
+}
